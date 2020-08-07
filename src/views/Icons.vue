8e061a448ec290bce16c5930739333e8d13bbd47
Omer Faruk Davarci <davarci16@itu.edu.tr>
2020-08-07 15:15:30 +0300

    Create Icons view

---
+++ src/views/Icons.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="icons">
+    <h1>Icons</h1>
+    <IconHome />
+    <IconSettings />
+  </div>
+</template>
+
+<script>
+import IconHome from '../icons/home.svg'
+import IconSettings from '../icons/settings.svg'
+
+export default {
+  name: 'Icons',
+  components: { IconHome, IconSettings }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
