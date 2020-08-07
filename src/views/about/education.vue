3d53a5e0e2ecddab8bb50bad717fbc3bc62b4c8d
Omer Faruk Davarci <davarci16@itu.edu.tr>
2020-08-07 15:57:15 +0300

    Add new routes for about page

---
+++ src/views/about/education.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="education">
+    <h1>Education</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Education'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
---
+++ src/views/about/project.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="project">
+    <h1>Projects</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Project'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
