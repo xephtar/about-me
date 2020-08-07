3dfcba02986d9d77db9b96e1d8349c7d9361e52b
Omer Faruk Davarci <davarci16@itu.edu.tr>
2020-08-08 01:24:39 +0300

    Modify navbar of header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,42 @@
 <template>
-  <header>
-    <IconHome /><router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <header class="header">
+    <Container class="header-inner">
+      <router-link to="/">Home</router-link>
+      <form>
+        <input type="text" placeholder="Search" />
+      </form>
+
+      <nav>
+        <router-link to="/">
+          <IconHome />
+          <IconHomeFill />
+        </router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </Container>
   </header>
 </template>
 
 <script>
 import IconHome from '@/icons/home.svg'
+import IconHomeFill from '@/icons/home-fill.svg'
+import Container from '@/components/Container'
+
 export default {
   name: 'MainHeader',
-  components: { IconHome }
+  components: { Container, IconHome, IconHomeFill }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.header {
+  background-color: yellow;
+}
+
+.header-inner {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  align-items: center;
+  padding: 10px;
+}
+</style>
