01ce72e89552f7a057957e7e479adae8fe8c6b3b
Omer Faruk Davarci <davarci16@itu.edu.tr>
2020-08-07 15:56:57 +0300

    Modify about page

---
+++ src/views/about/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <div id="nav">
+      <router-link to="/about/projects">Projects</router-link> |
+      <router-link to="/about/education">Education</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+  }
+}
+</style>
