3df52238ef1fefaa1c1fa79a20718bebc891947b
Omer Faruk Davarci <davarci16@itu.edu.tr>
2020-08-07 14:49:33 +0300

    Create styled component

---
+++ src/components/CustomText.vue
@@ -0,0 +1,53 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    },
+    size: {
+      type: String,
+      default: 'medium', // xlarge:18px, large:16px, medium:14px, small:12px, xsmall:11px, xxsmall:10px
+      validator: function(value) {
+        return (
+          ['xlarge', 'large', 'medium', 'small', 'xsmall', 'xxsmall'].indexOf(
+            value
+          ) !== -1
+        )
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.xlarge {
+  font-size: 18px;
+}
+
+.large {
+  font-size: 16px;
+}
+
+.medium {
+}
+
+.small {
+  font-size: 12px;
+}
+
+.xsmall {
+  font-size: 11px;
+}
+
+.xxsmall {
+  font-size: 10px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <CustomText tag="p" size="xlarge">XLarge Instance!</CustomText>
+    <CustomText tag="p" size="large">Large Instance!</CustomText>
+    <CustomText>Default!</CustomText>
+    <CustomText tag="p" size="small">Small Instance!</CustomText>
+    <CustomText tag="p" size="xsmall">XSmall Instance!</CustomText>
+    <CustomText tag="p" size="xxsmall">XXSmall Instance!</CustomText>
+
+    <CustomText class="a" tag="p">Colored Instance!</CustomText>
+    <CustomText class="b" tag="p">Colored Instance!</CustomText>
+    <CustomText class="c" tag="p">Colored Instance!</CustomText>
   </div>
 </template>
+
+<script>
+import CustomText from '@/components/CustomText'
+
+export default {
+  name: 'Home',
+  components: { CustomText },
+  comments: {
+    CustomText
+  }
+}
+</script>
 
 <style scoped>
 h1 {
@@ -11,5 +33,17 @@ h1 {
   @media (--t) {
     color: blue;
   }
+}
+
+.a {
+  color: rgba(var(--jbb), 1);
+}
+
+.b {
+  color: rgba(var(--ie3), 1);
+}
+
+.c {
+  color: rgba(var(--i30), 1);
 }
 </style>
