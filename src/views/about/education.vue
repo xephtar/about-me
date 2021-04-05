6becc0347b4dce9654243b84c54fdcb5f2092cb7
Omer Faruk Davarci <32176272+xephtar@users.noreply.github.com>
2021-04-05 15:34:38 +0300

    Update education.vue

--- src/views/about/education.vue
+++ src/views/about/education.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="education">
     <h1>Education</h1>
+    <button v-on:click="trackEvent">
+      {{ title }}
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Education'
+  name: 'Education',
+  data() {
+    return {
+      title: 'Track Button!'
+    }
+  },
+  methods: {
+    trackEvent () {
+      window.analytics.track('User Signup')
+    }
+  }
 }
 </script>
 
